d10b39e395672f1d1afaf05877143130880341dc
Derrick B <derrick_boston@student.davincischools.org>
2019-02-14 18:14:36 -0800

    Changes
    -Fixed indexing error with operator intake state indicators
    -Enabled encoder zeroing
    -Fixed setpoints working n manual mode

--- src/main/java/frc/robot/commands/SetAllMechanismSetpoints.java
+++ src/main/java/frc/robot/commands/SetAllMechanismSetpoints.java
@@ -13,6 +13,7 @@ import frc.robot.Robot;
 import frc.robot.RobotMap;
 import frc.robot.commands.elevator.SetElevatorSetpoint;
 import frc.robot.commands.wrist.SetWristSetpoint;
+import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Intake;
 
 /**
@@ -21,59 +22,62 @@ import frc.robot.subsystems.Intake;
 public class SetAllMechanismSetpoints extends CommandGroup {
     public SetAllMechanismSetpoints(int position) {
         // Set the position of the elevator
-        switch(position) {
-            case 4: // Rocket Level 3 Scoring Positions
-                if(Intake.outtakeState == 2) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_CARGO_HIGH_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_HIGH_POSITION));
-                } if(Intake.outtakeState == 1) {
-                addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
-                addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_HIGH_POSITION));
-                } else {
+        if(Elevator.controlMode == 1) {
+            switch (position) {
+                case 4: // Rocket Level 3 Scoring Positions
+                    if (Intake.outtakeState == 2) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_CARGO_HIGH_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_HIGH_POSITION));
+                    }
+                    if (Intake.outtakeState == 1) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_HIGH_POSITION));
+                    } else {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_HIGH_POSITION));
+                    }
+                case 3: // Rocket Level 2 Scoring Positions
+                    if (Intake.outtakeState == 2) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_MID_POSITION));
+                    } else if (Intake.outtakeState == 1) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_MID_POSITION));
+                    } else {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_MID_POSITION));
+                    }
+                    break;
+                case 2: // Rocket Level 1 Scoring Positions
+                    if (Intake.outtakeState == 2) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_LOW_POSITION));
+                    } else if (Intake.outtakeState == 1) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_LOW_POSITION));
+                    } else {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_LOW_POSITION));
+                    }
+                    break;
+                case 1: // Cargo Ship Scoring Positions
+                    if (Intake.outtakeState == 2) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_SHIP_POSITION));
+                    } else if (Intake.outtakeState == 1) {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_LOW_POSITION));
+                    } else {
+                        addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
+                        addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_LOW_POSITION));
+                    }
+                    break;
+                case 0: // Elevator, Wrist Home Position
+                default:
                     addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_HIGH_POSITION));
-                }
-            case 3: // Rocket Level 2 Scoring Positions
-                if(Intake.outtakeState == 2) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_MID_POSITION));
-                } else if(Intake.outtakeState == 1) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_MID_POSITION));
-                } else {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_MID_POSITION));
-                }
-                break;
-            case 2: // Rocket Level 1 Scoring Positions
-                if(Intake.outtakeState == 2) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_LOW_POSITION));
-                } else if(Intake.outtakeState == 1) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_LOW_POSITION));
-                } else {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_LOW_POSITION));
-                }
-                break;
-            case 1: // Cargo Ship Scoring Positions
-                if(Intake.outtakeState == 2) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_EXTENDED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_CARGO_SHIP_POSITION));
-                } else if(Intake.outtakeState == 1) {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_HATCH_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_GROUND_LOW_POSITION));
-                }else {
-                    addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
-                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HATCH_LOW_POSITION));
-                }
-                break;
-            case 0: // Elevator, Wrist Home Position
-            default:
-                addSequential(new SetWristSetpoint(RobotMap.WRIST_RETRACTED_ANGLE));
-                addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HOME_POSITION));
-                break;
+                    addSequential(new SetElevatorSetpoint(RobotMap.ELEVATOR_HOME_POSITION));
+                    break;
+            }
         }
     }
 }
