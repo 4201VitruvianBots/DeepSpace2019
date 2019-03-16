124a54c4826b04594d8d1e1329cdbd7555101471
Unknown <jonathandao0@gmail.com>
2019-03-16 11:02:48 -0700

    Changes
    Cherrypick of LED code

--- src/main/java/frc/robot/commands/LEDReaction.java
+++ src/main/java/frc/robot/commands/LEDReaction.java
@@ -26,22 +26,22 @@ public class LEDReaction extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        if(Robot.ledOutput.getShifterState){    //if we're in "high" gear
-            Robot.ledOutput.setPinOutput(false,0);   //first bit off
+        if(Robot.ledOutput.getShifterState){    //if we're in low(er) gear
+            Robot.ledOutput.setPinOutput(true,0);   //first bit on
         }
-        else{   //if we're in low(er) gear
-            Robot.ledOutput.setPinOutput(true,0);  //first bit on
+        else{   //if we're in "high" gear
+            Robot.ledOutput.setPinOutput(false,0);  //first bit on
         }
         switch (LEDOutput.LEDColour) {
             case 4:      //If we detect a successful hatch intake, should be green
-                Robot.ledOutput.setPinOutput(true,3);
+                Robot.ledOutput.setPinOutput(false,3);
                 Robot.ledOutput.setPinOutput(false,2);
-                Robot.ledOutput.setPinOutput(false,1);
+                Robot.ledOutput.setPinOutput(true,1);
                 break;
             case 3:      //If we're using the hatch intake, should be yellow
                 Robot.ledOutput.setPinOutput(true,3);
                 Robot.ledOutput.setPinOutput(true,2);
-                Robot.ledOutput.setPinOutput(true,1);
+                Robot.ledOutput.setPinOutput(false,1);
                 break;
             case 2:     //if we're using the cargo intake, should be red
                 Robot.ledOutput.setPinOutput(false,3);
@@ -49,9 +49,9 @@ public class LEDReaction extends Command {
                 Robot.ledOutput.setPinOutput(false,1);
                 break;
             case 1:     //if we're climbing, should be blue
-                Robot.ledOutput.setPinOutput(false,3);
+                Robot.ledOutput.setPinOutput(true,3);
                 Robot.ledOutput.setPinOutput(false,2);
-                Robot.ledOutput.setPinOutput(true,1);
+                Robot.ledOutput.setPinOutput(false,1);
                 break;
             default:    //should only really happen if robot is disabled
                 Robot.ledOutput.setPinOutput(false,3);
